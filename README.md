feb51b857c16738f3d21e73c45ca8d48242ed298
Tihh Gonçalves <tihh.gon@gmail.com>
2017-11-17 22:56:43 -0200

    up

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# tihh.php.fnc.friendlyLink
-Função que retorna uma URL amigável
+# Função de PHP - tihh_friendlyLink()
+Função em PHP que transforma qualquer string em um Link Amigável
+[![Versão](http://app.tiago.art.br/flags/version.php?path=tihhgoncalves/tihh.php.fnc.friendlyLink)](/releases.md)
+[![Versão](http://app.tiago.art.br/flags/size.php?path=tihhgoncalves/tihh.php.fnc.friendlyLink)](/releases.md)
+
+## Parâmetros
+ * ```$str``` - Palavra que será convertida em Link Amigável.
+
+### Instalação
+Para utilizar esse script, utilize o seguinte comando:
+
+```
+  bower install tihh.php.fnc.friendlyLink --save
+```
+
+P.S.: Caso prefira, você pode fazer o download do projeto manualmente.
+
+### Exemplo
+```
+  <?
+  
+  required('bower_components/tihh.php.fnc.friendlyLink/load.php');
+  
+  $texto - 'Olá mundo cruel';
+  $tags = tihh_friendlyLink($texto);
+  echo($tags); //ola-mundo-cruel
+  
+  ?>
+```
+### Autor
+![logo](https://raw.githubusercontent.com/tihhgoncalves/tihh.php.fnc.getTags/master/logo.png)
+
+Esse script de PHP foi desenvolvido por Tihh Gonçalves (tiago@tiago.art.br). 
+
+Mais informações: www.tiago.art.br
---
+++ load.php
@@ -0,0 +1,30 @@
+<?
+/**
+ * Função em PHP transforma qualquer string numa URL de link amigável
+ *
+ *
+ * Versão: 1.0
+ *
+ * @param $str
+ * @return string
+ */
+function tihh_friendlyLink($texto)
+{
+  $texto = trim($texto);
+
+  //Tira acentos
+  $comAcento = array('O','à','á','â','ã','ä','å','ç','è','é','ê','ë','ì','í','î','ï','ñ','ò','ó','ô','õ','ö','ù','ü','ú','ÿ','À','Á','Â','Ã','Ä','Å','Ç','È','É','Ê','Ë','Ì','Í','Î','Ï','Ñ','Ò','Ó','Ô','Õ','Ö','O','Ù','Ü','Ú','Ÿ','&', 'ª', 'º', '%');
+  $semAcento = array('o','a','a','a','a','a','a','c','e','e','e','e','i','i','i','i','n','o','o','o','o','o','u','u','u','y','A','A','A','A','A','A','C','E','E','E','E','I','I','I','I','N','O','O','O','O','O','0','U','U','U','Y','e', 'a', 'o', 'perc');
+  $texto = str_replace($comAcento, $semAcento, $texto);
+  //Anula alguns acaracters
+  $texto = str_replace(array('?', '!', ':', ';', '~', '`', '´', '{', '}', '[', ']', '/', '\\', ',', '(', ')', '"', '\'', '#'), '', $texto);
+  //Substitui espaços
+  $texto = str_replace(' ', '-', $texto);
+  //Eleminia ífens duplicados
+  $texto = str_replace('--', '-', $texto);
+  //Passa pra minúsculo
+  $texto = strtolower($texto);
+
+  return $texto;
+}
+?>
